3b9d2b3ce7b1409c1c3163aca3cee372bcb538a4
Kenan <mckenzie.kenan@yahoo.com>
2022-11-28 14:20:36 -0500

    added api route for saving notes and logging their id

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,3 +1,4 @@
+const saveNote = require('../public/assets/js/index')
 const router = require('express').Router();
 const fs = require('fs')
 
@@ -5,12 +6,11 @@ const fs = require('fs')
 router.get('/notes', (req, res) => {
     
 })
+
 //Clicking on link to notes page takes you to page w/ existing notes listed in the left column & empty fields for new notes on the right -> notes.html
 router.get('/notes:id', (req, res) => {
     const result = getNotes(req.params.id, noteId)
 })
-
-
 
 //GET /api/notes should read the db.json and return all saved notes as json
 router.get('/api/notes', (req, res) => {
@@ -25,6 +25,22 @@ router.get('/api/notes', (req, res) => {
     
 //POST /api/notes should receive a new note to save on the req.body -> add it to db.json file -> then return the new note to client
     //Each note should have unique id
+router.post('/api/notes', (req,res) => {
+    const newNote = req.body;
+    newNote.id = 0;
+    fs.readFile(__dirname + '/../db/db.json', (err, data) => {
+        if (err) {
+            console.log(err)
+        } else saveNote = JSON.parse(data);
+        for (var i = 0; i < saveNote.length; i++) {
+            if (saveNote[i].id > id) {
+                id = saveNote[i].id
+            }
+        }
+    })
+
+})
+
 
 //DELETE notes route
 
